d6000b59b3b2db91c33b9f16b676402771112670
Maria Daniela Martin <m.danielamartin@proton.me>
2025-07-26 01:34:52 -0400

    login and signup completed

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,66 +1,80 @@
 
 "use client"
-import { Button, Checkbox, Label, TextInput } from "flowbite-react";
+import { Button, Checkbox, Label, TextInput, Spinner } from "flowbite-react";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { useRouter } from 'next/navigation'
 import * as yup from "yup"
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useEffect } from "react";
+import useUserStore from "@/store/userStore";
 
-const SignUp = ()=> {
-    const router = useRouter()
-    const signupSchema = yup.object().shape({
-        email: yup.string().email("Invalid email format").required("Please type your email address"),
-        password: yup.string().min(8, "Password must be at least 8 characters long.").required("A password is required"),
-        passwordConfirm: yup.string().oneOf([yup.ref("password")],"Passwords must match").required("Please retype your password to register")
-    })
-    interface signupData {
-  email: string;
-  password: string;
-  confirmPassword: string;
-}
-    const {register, handleSubmit, formState:{errors}} = useForm({resolver: yupResolver(signupSchema)})
+const SignUp = () => {
+  const router = useRouter()
+  const { signup, isLoading } = useUserStore()
+  const signupSchema = yup.object().shape({
+    email: yup.string().email("Invalid email format").required("Please type your email address"),
+    password: yup.string().min(8, "Password must be at least 8 characters long.").required("A password is required"),
+    passwordConfirm: yup.string().oneOf([yup.ref("password")], "Passwords must match").required("Please retype your password to register")
+  })
+  interface signupData {
+    email: string;
+    password: string;
+    passwordConfirm: string;
+  }
+  const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(signupSchema) })
 
+  const onSubmit = async (data: signupData) => {
+    const res = await signup(data)
+    if (res) {
+      router.push("/dashboard")
+    } else {
+      console.log("something went wrong")
+    }
+  }
 
   return (
     <div className="flex h-screen items-center justify-center">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex max-w-lg w-full p-6 flex-col gap-4">
+        <h1 className="font-bold text-2xl text-center">REGISTER</h1>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="email2">Your email</Label>
+          </div>
+          <TextInput id="email2" type="email" placeholder="name@flowbite.com" required shadow {...register("email")} />
+          <p className="text-red-500">{errors.email?.message}</p>
 
-    <form  className="flex max-w-md max-w-lg w-full p-6 flex-col gap-4">
-         <h1 className="font-bold text-2xl text-center">REGISTER</h1>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="email2">Your email</Label>
         </div>
-        <TextInput id="email2" type="email" placeholder="name@flowbite.com" required shadow {...register("email")} />
-        <p className="text-red-500">{errors.email?.message}</p>
-
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="password2">Your password</Label>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="password2">Your password</Label>
+          </div>
+          <TextInput id="password2" type="password" required shadow {...register("password")} />
+          <p className="text-red-500">{errors.password?.message}</p>
         </div>
-        <TextInput id="password2" type="password" required shadow {...register("password")}/>
-        <p className="text-red-500">{errors.password?.message}</p>
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="repeat-password">Repeat password</Label>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="repeat-password">Repeat password</Label>
+          </div>
+          <TextInput id="repeat-password" type="password" required shadow {...register("passwordConfirm")} />
+          <p className="text-red-500">{errors.passwordConfirm?.message}</p>
         </div>
-        <TextInput id="repeat-password" type="password" required shadow {...register("passwordConfirm")} />
-        <p className="text-red-500">{errors.passwordConfirm?.message}</p>
-      </div>
-      <div className="flex items-center gap-2">
-        <Checkbox id="agree" />
-        <Label htmlFor="agree" className="flex">
-          I agree with the&nbsp;
-          <Link href="#" className="text-cyan-600 hover:underline dark:text-cyan-500">
-            terms and conditions
-          </Link>
-        </Label>
-      </div>
-      <Button type="submit">Register new account</Button>
-    </form>
+        <div className="flex items-center gap-2">
+          <Checkbox id="agree" />
+          <Label htmlFor="agree" className="flex">
+            I agree with the&nbsp;
+            <Link href="#" className="text-cyan-600 hover:underline dark:text-cyan-500">
+              terms and conditions
+            </Link>
+          </Label>
+        </div>
+        {isLoading ? (
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100px' }}>
+            <Spinner color="purple" />
+          </div>
+        ) : (
+          <Button color="purple" type="submit">Sign Up</Button>
+        )}
+      </form>
     </div>
   );
 }
--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,97 +1,95 @@
-
 import { create } from "zustand";
 
-
-
-
-const backendURL = process.env.NEXT_PUBLIC_BACKEND_URL
-
+const backendURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 interface UserState {
-    token: string | null;
-    error: string | null;
-    isLoading: boolean;
-    login: (data:loginFormat) => Promise<boolean>;
-    signup: (data:loginFormat) => Promise<boolean>
-    clearError: () => void
-    logout: () => void
-    printsomething: ()=>string
+  token: string | null;
+  error: string | null;
+  isLoading: boolean;
+  login: (data: loginFormat) => Promise<boolean>;
+  signup: (data: loginFormat) => Promise<boolean>;
+  clearError: () => void;
+  logout: () => void;
+  printsomething: () => string;
 }
-type loginFormat = {email:string,password:string}
-
+type loginFormat = { email: string; password: string };
 
 const useUserStore = create<UserState>((set) => ({
-    token: typeof window !== 'undefined' ? sessionStorage.getItem('token') : null,
-    error: null,
-    isLoading: false,
+  token: typeof window !== "undefined" ? sessionStorage.getItem("token") : null,
+  error: null,
+  isLoading: false,
 
-    login: async (loginData) => {
-        set({ isLoading: true, error: null })
-        try {
-            const res = await fetch(`${backendURL}user/login`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(loginData),
-            })
-            if (!res.ok) {
-                const errorData = await res.json()
-                throw new Error(errorData || 'Login failed')
-            }
-            const userData = await res.json()
-            const token = userData.access_token
-            sessionStorage.setItem("token", token)
-            set({ isLoading: false, token: token })
-            return true
-        } catch (err: unknown) {
-            let message = 'Unexpected error'
-            if (err instanceof Error) {
-                message = err.message
-            }
-            set({ error: message, isLoading: false })
-            return false
-        }
-    },
-    clearError: () => {
-        set({ error: null })
-    },
-
-    logout: () => {
-        set({token:null})
-        sessionStorage.clear()
-    },
-
-    signup: async (registerData) => {
-        set({ isLoading: true, error: null })
-        try {
-            const res = await fetch(`${backendURL}user/register`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(registerData),
-            })
-
-            if (!res.ok) {
-                const errorData = await res.json()
-                throw new Error(errorData || 'Login failed')
-            }
-            set({isLoading: false})
-            return true
-        } catch (err: unknown) {
-            let message = 'Unexpected error'
-            if (err instanceof Error) {
-                message = err.message
-            }
-            set({ error: message, isLoading: false })
-            return false
-        }
-    },
-
-    printsomething: () =>{
-
-        return "Hello from the store"
+  login: async (loginData) => {
+    set({ isLoading: true, error: null });
+    try {
+      const res = await fetch(`${backendURL}user/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(loginData),
+      });
+      if (!res.ok) {
+        const errorData = await res.json();
+        throw new Error(errorData || "Login failed");
+      }
+      const userData = await res.json();
+      const token = userData.access_token;
+      document.cookie = `Authorization=Bearer ${token}; Path=/; Secure; SameSite=Strict`;
+      set({ isLoading: false, token: token });
+      return true;
+    } catch (err: unknown) {
+      let message = "Unexpected error";
+      if (err instanceof Error) {
+        message = err.message;
+      }
+      set({ error: message, isLoading: false });
+      return false;
     }
+  },
+  clearError: () => {
+    set({ error: null });
+  },
 
+  logout: () => {
+    document.cookie =
+      "Authorization=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; Secure; SameSite=Strict";
+    set({ token: null });
+    sessionStorage.clear();
+  },
 
+  signup: async (registerData) => {
+    set({ isLoading: true, error: null });
+    try {
+      const res = await fetch(`${backendURL}user/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: registerData["email"],
+          password: registerData["password"],
+        }),
+      });
 
+      if (!res.ok) {
+        const errorData = await res.json();
+        throw new Error(errorData || "Login failed");
+      }
+      const userData = await res.json();
+      const token = userData.access_token;
+      document.cookie = `Authorization=Bearer ${token}; Path=/; Secure; SameSite=Strict`;
+      set({ isLoading: false });
+      return true;
+    } catch (err: unknown) {
+      let message = "Unexpected error";
+      if (err instanceof Error) {
+        message = err.message;
+      }
+      set({ error: message, isLoading: false });
+      return false;
+    }
+  },
+
+  printsomething: () => {
+    return "Hello from the store";
+  },
 }));
 
 export default useUserStore;
